6354e5b40e1842aac004965483d6b1281185806c
Ignat <mister.strays@gmail.com>
2021-03-09 18:14:21 +0300

    Make output window

---
+++ Calculator/DefaultSolve.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Calculator
+{
+    partial class DefaultSolve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableOut = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableOutFunc = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableOut
+            // 
+            this.tableOut.AutoScroll = true;
+            this.tableOut.BackColor = System.Drawing.SystemColors.Control;
+            this.tableOut.Location = new System.Drawing.Point(12, 12);
+            this.tableOut.Name = "tableOut";
+            this.tableOut.Size = new System.Drawing.Size(460, 571);
+            this.tableOut.TabIndex = 36;
+            // 
+            // tableOutFunc
+            // 
+            this.tableOutFunc.AutoScroll = true;
+            this.tableOutFunc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tableOutFunc.Location = new System.Drawing.Point(478, 12);
+            this.tableOutFunc.Name = "tableOutFunc";
+            this.tableOutFunc.Size = new System.Drawing.Size(460, 571);
+            this.tableOutFunc.TabIndex = 37;
+            // 
+            // DefaultSolve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 595);
+            this.Controls.Add(this.tableOutFunc);
+            this.Controls.Add(this.tableOut);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "DefaultSolve";
+            this.Text = "Стандартное решение";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel tableOut;
+        private System.Windows.Forms.FlowLayoutPanel tableOutFunc;
+    }
+}
---
+++ Calculator/DefaultSolve.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class DefaultSolve : Form
+    {
+        string str;
+        VariablesAlphabet variablesAlphabet = new VariablesAlphabet();
+        SignsAlphabet signsAlphabet = new SignsAlphabet();
+        InputString inputString;
+        Table table;
+        TableSolved tableSolved;
+        Solve solve = new Solve();
+
+        List<List<TableCell>> _table = new List<List<TableCell>>();
+
+
+        public DefaultSolve(string input)
+        {
+            InitializeComponent();
+            str = input;
+            inputString = new InputString(input);
+            table = new Table(Math.Pow(2, inputString.GetVariablesCount(str)), inputString.GetVariablesCount(str), str);
+            tableOut.Size = new Size(inputString.GetVariablesCount(str) * 50 + 15 * inputString.GetVariablesCount(str), 
+                tableOut.Size.Height);
+
+            tableSolved = new TableSolved(inputString.GetStepsCount(str), inputString.GetStepsCount(str), str,
+                table);
+            tableOutFunc.Size = new Size(inputString.GetStepsCount(str) * 50 + 15 * inputString.GetStepsCount(str),
+                tableOut.Size.Height);
+
+
+            Solve();
+        }
+
+        private List<Control> GetChildren(Control control = null)
+        {
+            if (control == null) control = tableOut;
+
+            List<Control> list = control.Controls.OfType<Control>().ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Control child = list[i];
+                list.AddRange(GetChildren(child));
+            }
+
+            return list;
+        }
+
+        private void Solve()
+        {
+            for (int i = 0; i < table.GetRows() + 1; i++)
+            {
+                for (int j = 0; j < table.GetColumns(); j++)
+                {
+                    tableOut.Controls.Add(table.GetCell(i, j));
+                }
+            }
+
+            for (int i = 0; i < tableSolved.GetRows() + 1; i++)
+            {
+                for (int j = 0; j < tableSolved.GetColumns(); j++)
+                {
+                    tableOutFunc.Controls.Add(tableSolved.GetCell(i, j));
+                }
+            }
+        }
+    }
+
+
+}
